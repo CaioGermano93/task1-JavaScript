d1e90b693f1855bfb854b050de8de3a91f20d63a
Leesin32109 <caiogermano93@gmail.com>
2022-03-25 05:19:31 -0300

    Script js

---
+++ script.js
@@ -0,0 +1,148 @@
+
+// Estrtura: Sequencial
+
+// Questão 1
+
+// let nome = prompt("Digite o seu nome: ")
+// alert("Seu nome é: " + nome)
+// console.log(nome)
+
+// Questão 2
+
+// let num1 = parseInt(prompt("Digite o primeiro número"))
+// let num2 = parseInt(prompt("Digite o segundo número"))
+// console.log(num1+num2)
+
+// alert("A soma dos números é: " + (num1+num2))
+
+// Questão 3
+
+// let nota1 = parseInt(prompt("Digite sua primeira nota "))
+// let nota2 = parseInt(prompt("Digite sua segunda nota"))
+// console.log(nota1 + nota2 / 2)
+
+// alert("Sua média é: " + (nota1 + nota2) / 2)
+
+// Questão 4
+
+// let n=parseInt(prompt("Digite o ano em que você nasceu: "))
+// let at = parseInt(prompt("Digite o ano atual em que estamos: "))
+// console.log (at - n)
+
+// alert("Sua idade é: " + (at - n))
+
+// Questão 5
+
+// let sh = parseInt(prompt("Digite quanto você recebe por hora: "))
+// let ht = parseInt(prompt("Agora por favor digite quantas horas você trabalha em um mês: "))
+
+// console.log(sh * ht)
+
+// alert("Você recebe por mês = " + (sh * ht))
+
+// Questão 6
+
+// let f = parseInt(prompt("Por favor digite a temperatura em Fahrenheit: "))
+// console.log("A conversão da temperatura é: " + (C = 5 * (f-32) / 9 ))
+// alert("Temperatura em celcius= " + (C = 5 * (f-32) / 9 )) 
+
+// ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+
+// // Estrtura: Decisão
+
+// Questão 1
+
+// let num1 = parseInt(prompt("Digite o primeiro número: "))
+// let num2 = parseInt(prompt("Digite o segundo número: "))
+
+//     if  (num1 > num2){
+//        console.log("num1 é o maior: " + num1)
+//        alert("num1 é o maior: " + num1)
+//        }else {
+//        console.log("num2 é o maior: " + num2)
+//        alert("num2 é o maior: " + num2)
+//        }   
+    
+
+// Questão 2
+
+// let num = parseInt(prompt("Digite um número: "))
+
+// if (num >= 0){
+//     console.log("O número digitado é positivo ")
+//     alert("O número que você digitou é positivo")
+// }else {
+//     console.log("O número que você digitou é negativo")
+//     alert("O número que você digitou é negativo")
+// }
+ 
+// Questão 3
+
+// let genero = prompt("Digite f ou m para definir o seu sexo: ")
+//     if (genero === "m"){
+//         console.log("Seu sexo é masculino")
+//         alert("Seu sexo é masculino ")
+//     }else if (genero === "f") {
+//         console.log("Seu sexo é feminino")
+//         alert("Seu sexo é feminino ")
+//     }else{
+//         console.log("Sexo inválido, por favor digite f ou m: ")
+//         alert("Sexo inválido, por favor digite f ou m: ")
+//     }
+ 
+// Questão 4
+
+//    let nota1 = parseInt(prompt("Digite sua primeira nota: "))
+//    let nota2 = parseInt(prompt("Digite sua segunda nota"))
+   
+//    let media = ((nota1 + nota2)/2)
+//    console.log(nota1 + nota2)
+
+//      if (media  === 10){ 
+//         console.log("Parabéns você foi aprovado com distinção ")
+//         alert("Parabéns você foi aprovado com distinção")
+//      }else if (media > 10){
+//         console.log("Por favor digite uma nota válida")
+//         alert("Por favor digite uma nota válida")
+//      }else if(media < 7){
+//          console.log("Infelizmente você foi reprovado")
+//          alert("Infelizmente você foi reprovado")
+//      }else if(media  >= 7){
+//         console.log("Parabéns você foi aprovado")
+//         alert("Parabéns você foi aprovado")
+//      }else if (media > 10){
+//         console.log("Por favor digite uma nota válida")
+//         alert("Por favor digite uma nota válida")
+//      }
+    
+
+// Questão 5
+
+//   let num1 = parseInt(prompt("Digite o primeiro número: "))
+//   let num2 = parseInt(prompt("Digite o segundo número: "))
+//   let num3 = parseInt(prompt("Digite o terceiro número: "))
+
+//      let nMaior = Math.max(num1, num2, num3)
+//      let nMenor = Math.min(num1, num2, num3)
+//         console.log("O número maior é: " + nMaior +  ", e o número menor é: " + nMenor)
+
+// Questão 6
+
+// let num = parseInt(prompt("Digite um número: "))
+
+// if (num % 2 === 0){
+//     console.log("Esse número é par")
+//     alert("Esse número é par")
+// }else{
+//     console.log("Esse número é impar")
+//     alert("Esse número é impar")
+// }
+
+// ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+
+     
+     
+
+
